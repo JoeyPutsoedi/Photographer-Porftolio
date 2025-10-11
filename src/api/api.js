4a008c4a0f4934efbe10e47b9a0012679b6aa2bd
Joey Putsoedi <joeyputsoedi6@gmail.com>
2025-10-11 20:38:05 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://creativerori-api.onrender.com/",
+  baseURL: "https://creativerori-api.onrender.com/api/",
 });
 
 //fetch categories
