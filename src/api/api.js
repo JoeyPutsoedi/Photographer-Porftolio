f2391d17830f2b26b14d30b6e5d2b175a19ca4a6
Joey Putsoedi <joeyputsoedi6@gmail.com>
2025-10-11 20:15:11 +0200

    Update production API base URL
    Changed the production base URL from mygallery-api to creativerori-api to point to the new API endpoint.

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "https://mygallery-api.onrender.com/api",
+      : "https://creativerori-api.onrender.com/",
 });
 
 //fetch categories
