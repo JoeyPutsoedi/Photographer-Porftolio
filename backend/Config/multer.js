f0c00b4bd855f56281499471b0bf6a39adf855ad
Joey Putsoedi <joeyputsoedi6@gmail.com>
2025-11-04 18:00:38 +0200

    Update multer.js

--- backend/Config/multer.js
+++ backend/Config/multer.js
@@ -3,6 +3,12 @@ import multer from "multer";
 // store files in memory, then send buffer to Cloudinary
 const storage = multer.memoryStorage();
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10MB per file
+    files: 10, // maximum 10 files
+  },
+});
 
 export default upload;
